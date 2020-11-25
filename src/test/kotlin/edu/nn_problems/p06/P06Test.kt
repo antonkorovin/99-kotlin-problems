890ebfdc4044e21e0adb7d8e7f3cac24ed569e85
Anton Korovin <korovin.anton@gmail.com>
2020-11-25 13:02:11 +0200

    [p06] Add more tests

--- src/test/kotlin/edu/nn_problems/p06/P06Test.kt
+++ src/test/kotlin/edu/nn_problems/p06/P06Test.kt
@@ -3,12 +3,21 @@ package edu.nn_problems.p06
 import io.kotest.core.spec.style.FunSpec
 import io.kotest.matchers.shouldBe
 
+typealias Func<T> = (List<T>) -> Boolean
+
 class P06Test : FunSpec({
 
+    fun checkFun(f: Func<Int>) {
+        f(listOf(1, 2, 3, 2, 1)) shouldBe true
+        f(listOf(1, 2, 2, 1)) shouldBe true
+        f(listOf(1, 2, 3, 2, 42)) shouldBe false
+        f(listOf(42, 2, 3, 2, 1)) shouldBe false
+        f(listOf(42)) shouldBe true
+        f(listOf()) shouldBe false
+    }
+
     test("isPalindrome") {
-        isPalindrome(listOf(1, 2, 3, 2, 1)) shouldBe true
-        isPalindrome(listOf(1, 2, 3, 2, 42)) shouldBe false
-        isPalindrome(listOf(42, 2, 3, 2, 1)) shouldBe false
+        checkFun(::isPalindrome)
     }
 
 })
